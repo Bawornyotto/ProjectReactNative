d513db01560fd6071f4b7eef489380bc13548034
Bawornyotto <94946315+Bawornyotto@users.noreply.github.com>
2021-12-15 11:41:53 +0700

    Add files via upload

---
+++ 15-12-2021/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text,StyleSheet } from 'react-native'
+//import AlertExample from './components/AlertExample'
+import ModelExample from './components/ModelExample'
+const App = () => {
+  return (
+      //<AlertExample/>
+      <ModelExample/>
+  )
+}
+
+export default App
